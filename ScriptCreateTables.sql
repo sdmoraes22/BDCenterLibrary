0689a44951fac60a984406fadaf0c2723ad72ed8
Cristiano Moraes <cmoraes.ti@gmail.com>
2019-02-13 23:09:19 -0200

    Criação das páginas de alteração e inserção de Livros. e Script de Modelagem do banco de dados

---
+++ ScriptCreateTables.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE autor 
+(
+    id NUMBER NOT NULL,
+    nome VARCHAR2(80) NOT NULL,
+    CONSTRAINT autor_pk PRIMARY KEY(ID)
+);
+
+CREATE TABLE livro
+(
+    id NUMBER NOT NULL,
+    isbn NUMBER(13) NOT NULL UNIQUE,
+    nome VARCHAR2(80) NOT NULL,
+    preco NUMBER(10,2) NOT NULL,
+    data_publicacao DATE NOT NULL,
+    img_capa VARCHAR2(80) NOT NULL,
+    autor_id NUMBER NOT NULL,
+    CONSTRAINT livro_pk PRIMARY KEY(ID),
+    CONSTRAINT autor_fk 
+    FOREIGN KEY (autor_id)
+    REFERENCES autor(id)
+);
